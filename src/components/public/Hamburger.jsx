16089beeebf5e1008bb2a8b36610df04d468b569
Mirumo <mysteryperson520@gmail.com>
2021-03-15 16:19:54 +0800

    [Optimize] optimize hamburger display

--- src/components/public/Hamburger.jsx
+++ src/components/public/Hamburger.jsx
@@ -1,31 +1,54 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import '../../assets/scss/public/hamburger.scss';
 
 function Hamburger(props) {
-    const [active, setactive] = useState(false);
-    useEffect(() => {
-        props.callback(active);
-    },
-        // eslint-disable-next-line
-        []
-    );
-    const handleClick = (e) => {
-        const isActive = active;
-        setactive(!isActive);
-        if (props.callback) props.callback(!isActive);
-    }
-    return (
-        <button
-            id="__hamburger__"
-            className={`${props.className} __hamburger__`}
-            onClick={handleClick}
-        >
-            <div className={`item${active ? ' active' : ''}`}></div>
-            <div className={`item${active ? ' active' : ''}`}></div>
-            <div className={`item${active ? ' active' : ''}`}></div>
-        </button>
-
-    )
+	const [active, setactive] = useState(false);
+	useEffect(
+		() => {
+			props.callback(active);
+		},
+		// eslint-disable-next-line
+		[]
+	);
+	const handleClick = (e) => {
+		let isActive;
+		if (props.active !== undefined) {
+			isActive = props.active;
+		} else {
+			isActive = active;
+		}
+		setactive(!isActive);
+		if (props.callback) props.callback(!isActive);
+	};
+	return (
+		<button
+			id="__hamburger__"
+			className={`${props.className} __hamburger__`}
+			onClick={handleClick}
+		>
+			{props.active !== undefined ? (
+				<Fragment>
+					<div className={`item${active ? ' active' : ''}`}></div>
+					<div className={`item${active ? ' active' : ''}`}></div>
+					<div className={`item${active ? ' active' : ''}`}></div>
+				</Fragment>
+			) : (
+				<Fragment>
+					(
+					<div
+						className={`item${props.active ? ' active' : ''}`}
+					></div>
+					<div
+						className={`item${props.active ? ' active' : ''}`}
+					></div>
+					<div
+						className={`item${props.active ? ' active' : ''}`}
+					></div>
+					)
+				</Fragment>
+			)}
+		</button>
+	);
 }
 
 export default Hamburger;
