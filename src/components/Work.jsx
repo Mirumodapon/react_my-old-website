9a180732bafd11a0eb4a7a33e21dcfd12545e28d
Mirumo <mysteryperson520@gmail.com>
2021-03-16 15:03:23 +0800

    [Add] work page css

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,42 +1,51 @@
 import { useEffect, useState } from 'react';
 import WorkCard from './public/WorkCard';
+import '../assets/scss/works.scss';
 function Work() {
-    const [works, setWorks] = useState([]);
-    useEffect(() => {
-        console.log(0)
-        fetch('https://api.github.com/users/Mirumodapon/repos').then(
-            e => e.json()
-        ).then(
-            response => {
-                let _works = [];
-                response.forEach(
-                    item => {
-                        const {
-                            name,
-                            id,
-                            html_url,
-                            description,
-                            language,
-                            forks_count,
-                            open_issues,
-                            stargazers_count,
-                            homepage
-                        } = item;
-                        _works.push({ name, id, html_url, description, language, forks_count, open_issues, stargazers_count, homepage });
-                    }
-                )
-                setWorks(_works);
-            }
-        )
-        // eslint-disable-next-line
-    }, [])
-    return (
-        <main>
-            <div>
-                {works.map(item => <WorkCard key={item.id} data={item}></WorkCard>)}
-            </div>
-        </main>
-    );
+	const [works, setWorks] = useState([]);
+	useEffect(() => {
+		console.log(0);
+		fetch('https://api.github.com/users/Mirumodapon/repos')
+			.then((e) => e.json())
+			.then((response) => {
+				let _works = [];
+				response.forEach((item) => {
+					const {
+						name,
+						id,
+						html_url,
+						description,
+						language,
+						forks_count,
+						open_issues,
+						stargazers_count,
+						homepage
+					} = item;
+					_works.push({
+						name,
+						id,
+						html_url,
+						description,
+						language,
+						forks_count,
+						open_issues,
+						stargazers_count,
+						homepage
+					});
+				});
+				setWorks(_works);
+			});
+		// eslint-disable-next-line
+	}, []);
+	return (
+		<main id="works">
+			<div className="works">
+				{works.map((item) => (
+					<WorkCard key={item.id} data={item}></WorkCard>
+				))}
+			</div>
+		</main>
+	);
 }
 
 export default Work;
