9985432b0bd3b9c2ab9b2ade536d0a7752bdd73b
mirumo <mysteryperson520@gmail.com>
2021-04-28 23:28:35 +0800

    [feat] add switch button component

---
+++ src/components/public/SwitchButton.jsx
@@ -0,0 +1,13 @@
+import '../../assets/scss/public/switchbutton.scss';
+
+function SwitchButton({ callback, checked }) {
+    const handleChange = (e) => e.target.checked;
+    return (
+        <label className="switch">
+            <input type="checkbox" onChange={callback ? callback : handleChange} checked={checked} />
+            <span className="slider"></span>
+        </label>
+    );
+}
+
+export default SwitchButton;
