80d2b0d4122b9a3b2061ead3f56ffc2cffc6ab7c
Mirumo <mysteryperson520@gmail.com>
2021-04-30 19:12:12 +0800

    [feat] add tools list

--- src/components/Tool.jsx
+++ src/components/Tool.jsx
@@ -1,12 +1,32 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import SM from './features/SM';
 import Qr from './features/QR';
 
-function Tool() {
+import '../assets/scss/tools.scss';
 
+function ToolList() {
+	return (
+		<div className="toolslist">
+			<div className="container">
+				<Link to="/tools/sm">
+					<span>S</span>Screen Message
+				</Link>
+				<Link to="/tools/qr">
+					QR code Tools<span>Q</span>
+				</Link>
+				<Link to="/tools/ran">
+					<span>R</span>Random Generater
+				</Link>
+			</div>
+		</div>
+	);
+}
+
+function Tool() {
 	return (
 		<Switch>
+			<Route path="/tools" component={ToolList} exact></Route>
 			<Route path="/tools/qr" component={Qr}></Route>
 			<Route path="/tools/sm" component={SM}></Route>
 		</Switch>
