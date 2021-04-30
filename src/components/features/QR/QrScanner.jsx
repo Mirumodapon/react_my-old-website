742323e38ba85f7503044820181608b200a07218
Mirumo <mysteryperson520@gmail.com>
2021-04-30 18:07:49 +0800

    [feat & style] show the error message and style the message text

--- src/components/features/QR/QrScanner.jsx
+++ src/components/features/QR/QrScanner.jsx
@@ -1,36 +1,39 @@
 import { useState, Fragment } from 'react';
 import QrReader from 'react-qr-reader';
 
-
 function QrScanner() {
-    const [delay] = useState(1000);
-    const [data, setData] = useState('No Result');
-    const [isLink, setIsLink] = useState(false);
-    const handleError = (e) => {
-        console.log(e);
-    }
-    const handleScan = (d) => {
-        if (d) {
-            setData(d);
-            const reg = /^(https)|(http)/;
-            setIsLink(reg.exec(d) ? true : false);
-        }
-    }
-    return (
-        <Fragment>
-            <QrReader
-                className="qrScanner"
-                delay={delay}
-                onError={handleError}
-                onScan={handleScan}
-            />
-            <p className="qrResult">
-                {data}
-                <br />
-                {isLink && <a href={data}>Go to Link</a>}
-            </p>
-        </Fragment>
-    )
+	const [delay] = useState(1000);
+	const [data, setData] = useState('No Result');
+	const [isLink, setIsLink] = useState(false);
+	const [showScanner, setShowScanner] = useState(true);
+	const handleError = (e) => {
+		setShowScanner(false);
+		setData(e.message);
+	};
+	const handleScan = (d) => {
+		if (d) {
+			setData(d);
+			const reg = /^(https)|(http)/;
+			setIsLink(reg.exec(d) ? true : false);
+		}
+	};
+	return (
+		<Fragment>
+			{showScanner && (
+				<QrReader
+					className="qrScanner"
+					delay={delay}
+					onError={handleError}
+					onScan={handleScan}
+				/>
+			)}
+			<p className="qrResult">
+				{data}
+				<br />
+				{isLink && <a href={data}>Go to Link</a>}
+			</p>
+		</Fragment>
+	);
 }
 
 export default QrScanner;
