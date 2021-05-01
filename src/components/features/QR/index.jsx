497e5b62bf84d5c9bf71bd87a49ed71e2e983bf4
Mirumo <mysteryperson520@gmail.com>
2021-05-01 16:29:11 +0800

    [refact] qr component without url params

--- src/components/features/QR/index.jsx
+++ src/components/features/QR/index.jsx
@@ -1,30 +1,34 @@
-import { Fragment, useState, useEffect } from 'react';
-import { Link, matchPath, useLocation } from 'react-router-dom';
+import { Fragment, useState } from 'react';
 
 import QrScanner from './QrScanner';
 import QrGenerate from './QrGenerate';
 import '../../../assets/scss/qr.scss';
 
 function Qr() {
-    const [mode, setMode] = useState(null);
-    const location = useLocation();
-    const match = matchPath(location.pathname, '/tools/qr/:mode');
-    useEffect(() => {
-        if (match)
-            setMode(match.params.mode);
-        else
-            setMode('scan');
-    }, [match])
-    return (
-        <Fragment>
-            {mode === 'scan' && <QrScanner></QrScanner>}
-            {mode === 'gen' && <QrGenerate></QrGenerate>}
-            <div className="qr-mode-btn">
-                <Link to="/tools/qr/scan" className={mode === 'scan' ? 'active' : ''}>Scanner</Link>
-                <Link to="/tools/qr/gen" className={mode === 'gen' ? 'active' : ''}>Genetate</Link>
-            </div>
-        </Fragment>
-    )
+	const [mode, setMode] = useState(null);
+	const handleMode = (e) => {
+		setMode(e.target.innerHTML);
+	};
+	return (
+		<Fragment>
+			{mode === 'Scanner' && <QrScanner></QrScanner>}
+			{mode !== 'Scanner' && <QrGenerate></QrGenerate>}
+			<div className="qr-mode-btn">
+				<span
+					onClick={handleMode}
+					className={mode !== 'Scanner' ? 'active' : ''}
+				>
+					Generate
+				</span>
+				<span
+					onClick={handleMode}
+					className={mode === 'Scanner' ? 'active' : ''}
+				>
+					Scanner
+				</span>
+			</div>
+		</Fragment>
+	);
 }
 
 export default Qr;
